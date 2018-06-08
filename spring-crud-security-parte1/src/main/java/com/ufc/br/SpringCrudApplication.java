2d2f1840b1bd481437277d98aec79aef4ea750f3
julioserafim <serafimjuliom@gmail.com>
2018-06-08 11:39:50 -0300

    Configuração do Security, Autenticacao via JPA + formulario de login

---
+++ spring-crud-security-parte1/src/main/java/com/ufc/br/SpringCrudApplication.java
@@ -0,0 +1,12 @@
+package com.ufc.br;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCrudApplication.class, args);
+	}
+}
---
+++ spring-crud-security-parte1/src/main/java/com/ufc/br/repository/PessoaRepository.java
@@ -0,0 +1,13 @@
+package com.ufc.br.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ufc.br.model.Pessoa;
+
+@Repository
+public interface PessoaRepository extends JpaRepository<Pessoa, Long>{
+	
+	Pessoa findByLogin(String login);
+
+}
