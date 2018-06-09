1169874df549199c19d81736c8b41c44df12b256
julioserafim <serafimjuliom@gmail.com>
2018-06-08 21:06:56 -0300

    Configuracao do Security, Autenticacao via JPA + formulario de login

---
+++ spring-crud-security-part1/src/main/java/com/ufc/br/model/Pessoa.java
@@ -0,0 +1,111 @@
+package com.ufc.br.model;
+
+import java.util.Collection;
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+
+@Entity
+public class Pessoa implements UserDetails{
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private String nome;
+	private String time;
+	private String login;
+	private String senha;
+	
+	@DateTimeFormat(pattern = "dd/MM/yyyy")
+	@Temporal(TemporalType.DATE)
+	private Date dataNascimento;
+	
+	
+	
+	public Date getDataNascimento() {
+		return dataNascimento;
+	}
+	public void setDataNascimento(Date dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getTime() {
+		return time;
+	}
+	public void setTime(String time) {
+		this.time = time;
+	}
+	
+	
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public String getPassword() {
+		// TODO Auto-generated method stub
+		return this.senha;
+	}
+	@Override
+	public String getUsername() {
+		// TODO Auto-generated method stub
+		return this.login;
+	}
+	@Override
+	public boolean isAccountNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+	
+	
+
+}
---
+++ spring-crud-security-part1/src/main/java/com/ufc/br/security/UserDetailsServiceImplementacao.java
@@ -0,0 +1,30 @@
+package com.ufc.br.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Repository;
+
+import com.ufc.br.model.Pessoa;
+import com.ufc.br.repository.PessoaRepository;
+
+@Repository
+public class UserDetailsServiceImplementacao implements UserDetailsService{
+	@Autowired	
+	private PessoaRepository pessoaRepository;
+	
+	
+	@Override
+	public UserDetails loadUserByUsername(String login) throws UsernameNotFoundException {
+		Pessoa pessoa = pessoaRepository.findByLogin(login);
+		
+		if(pessoa == null) {
+			throw new UsernameNotFoundException("Deu merda");
+		}
+		
+		
+		return pessoa;
+	}
+
+}
